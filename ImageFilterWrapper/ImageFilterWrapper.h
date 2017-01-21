13eeeeaf5534cb83ab5c7abe591289edee8c0598
Andrew Dempsey <amd0331@gmail.com>
2017-01-21 11:58:41 -0800

    Initial Commit

---
+++ ImageFilterWrapper/ImageFilterWrapper.h
@@ -0,0 +1,19 @@
+//
+//  ImageFilterWrapper.h
+//  ImageFilterWrapper
+//
+//  Created by Andrew Dempsey on 1/21/17.
+//  Copyright © 2017 dempsey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ImageFilterWrapper.
+FOUNDATION_EXPORT double ImageFilterWrapperVersionNumber;
+
+//! Project version string for ImageFilterWrapper.
+FOUNDATION_EXPORT const unsigned char ImageFilterWrapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImageFilterWrapper/PublicHeader.h>
+
+
