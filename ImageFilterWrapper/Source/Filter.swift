3814b7a10456277e7fa1e06ad73410c0ebe188e9
Andrew Dempsey <amd0331@gmail.com>
2019-01-13 14:42:37 -0500

    Move setOptions method to separate CIFilter extension file

--- ImageFilterWrapper/Source/Filter.swift
+++ ImageFilterWrapper/Source/Filter.swift
@@ -251,28 +251,3 @@ public enum ImageFilter {
 
 
 }
-
-extension CIFilter {
-
-    /**
-     Applies a set of configuration parameters to the current image filter.
-
-     - Parameter options: A set of tuples, where each tuple contains a key value pair. The key
-     must correspond to an enumerated `FilterOption`, and the value's type should match the
-     specification in the CIFilter documentation for the given parameter. Options are applied in
-     the given order.
-     */
-    internal func setOptions(_ options: (value: Any?, key: FilterOption)...) {
-        for option in options {
-            if let value = option.value {
-
-                if let convertibleValue = value as? CoreImageConvertible {
-                    self.setValue(convertibleValue.coreImageFormat(), forKey: option.key.rawValue)
-                } else {
-                    self.setValue(value, forKey: option.key.rawValue)
-                }
-            }
-        }
-    }
-
-}
